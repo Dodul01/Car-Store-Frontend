4a3788f224d73b2cd6979923fe1ad13ef9b3aa63
Dodul01 <mhdodul299@gmail.com>
2025-01-31 15:14:32 +0600

    url redirect issue solve

--- src/components/FeaturedProducts/FeaturedProducts.tsx
+++ src/components/FeaturedProducts/FeaturedProducts.tsx
@@ -20,26 +20,27 @@ const FeaturedProducts = () => {
         >
           <div>
             <h1 className="title">Featured Products</h1>
-            <p className="subtitle">
-              Lorem ipsum is simply dummy text of the printing and typesetting
-              industry.
-            </p>
+            <p className="subtitle">Best cars in our collection</p>
           </div>
           <div>
-            <Button onClick={() => navigate('/all-cars')}>View All</Button>
+            <Button onClick={() => navigate("/all-cars")}>View All</Button>
           </div>
         </div>
 
         <div className="featured-products-container">
           {data?.data?.slice(0, 6).map((product: any) => (
-            <div style={{height: '300px' }} className="card" key={product.id}>
+            <div style={{ height: "300px" }} className="card" key={product.id}>
               <h2>
                 {product.brand} {product.model}
               </h2>
               <p>
                 ${product.price} <span></span>
               </p>
-              <img style={{height: "180px", objectFit: "cover"}} src={product.image} alt={product.name} />
+              <img
+                style={{ height: "180px", objectFit: "cover" }}
+                src={product.image}
+                alt={product.name}
+              />
               <div
                 onClick={() => navigate(`/${product._id}`)}
                 className="arrow"
